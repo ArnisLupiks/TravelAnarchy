c71e6794c051ca8a1bd5215447127b26211902dc
Arnis <arnislupiks@gmail.com>
2015-03-19 02:39:55 +0000

    changed login function
    changed login function

--- partials/login/login.js
+++ partials/login/login.js
@@ -10,8 +10,6 @@ angular.module( 'sample.login', [
       $location.path("/");
       $scope.$root.isAuthenticated = true;
       //storing user in database
-      getUsers();
-      function getUsers() {
              var formData = {
              uid: profile.user_id,
              name: profile.name,
@@ -21,19 +19,22 @@ angular.module( 'sample.login', [
              birthday: profile.birthday,
             picture: profile.picture
            };
-           console.log(formData);
-           $http({
-               url: "api/addUser.php",
-               data: formData,
-               method: 'POST',
-               headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
-           }).success(function(data){
+           console.log("a here",formData);
+           $scope.method = 'POST';
+           $scope.url = 'api/addUser.php';
+           //execute method
+           $http({method: $scope.method, url: $scope.url, data: formData, headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}}).
+             success(function(data, status) {
                //if added information, it will log OK and redirect.
                console.log("OK", data);
                //redirect to main post page
                $location.path("/");
-           }).error(function(err){"ERR", console.log(err)})
-       };
+           }).error(function(data, status) {
+               $scope.status = status;
+               console.log("oh, something whent wrong: " , data);
+           });
+
+
     }, function(error) {
       console.log("There was an error logging in", error);
     });
