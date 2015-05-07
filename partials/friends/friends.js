efcec83995d0949bc86b9f20083ac663b7d4d9f9
Arnis <arnislupiks@gmail.com>
2015-05-07 16:52:48 +0100

    Displays friends of the user
    in Friends page now is displayed list of friends that use has.

--- partials/friends/friends.js
+++ partials/friends/friends.js
@@ -13,19 +13,22 @@ angular.module('sample.friends', ['auth0'])
   return function(friendData, callback){
     $http.post('api/addFriend.php', friendData).
     success(function(data,status){
-      var message = "<strong> "+auth.profile.given_name+"</strong> " + data + " !!!";
+      var message = "<strong> "+auth.profile.given_name+"</strong> " + data;
       Flash.create('success', message, 'customAlert');
     })
   }
 })
 // ****** getting all Friends from database ***********
 .factory('allFriends',function($http){
-  return function(myDetails, data){
-      $http.post('api/getAllFriends.php', myDetails).
-      success(function(data){
-        console.log("This is Friends data", data);
+  return{
+    getFriends : function(myDetails){
+      return $http({
+        url: 'api/getAllFriends.php',
+        method: 'POST',
+        data: myDetails
       })
-  };
+    }
+  }
 })
 //********* Add friend controller *********************
 .controller('friendsCtrl', function ($scope, members, addFriend, $rootScope, $http, $filter, $window, $location, auth){
@@ -47,10 +50,12 @@ angular.module('sample.friends', ['auth0'])
         });
 })
 .controller('allFriendsCtrl', function($scope, allFriends, $rootScope, auth){
-    $scope.init = function(){
-      var myDetails = {userID: auth.profile.user_id};
-      allFriends(myDetails);
+  $scope.buddys = [];
+  var myDetails = {userID: auth.profile.user_id};
 
-    $scope.buddys = $rootScope.friends;
-  }
+  allFriends.getFriends(myDetails).success(function(data){
+    $scope.buddys = data;
+    console.log($scope.buddys);
+  });
+
 });
